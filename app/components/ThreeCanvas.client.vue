38b0d29b2e56e9c73ab50bc22c72c8e72be0e06e
PumpkinPeasant <m2314670@edu.misis.ru>
2025-09-19 23:43:31 +0300

    Cubes

---
+++ app/components/ThreeCanvas.client.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import type { BufferGeometry} from "three";
+import {
+  BoxGeometry,
+  DirectionalLight,
+  Mesh,
+  MeshPhongMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer
+} from "three";
+
+const canvasRef: Ref<HTMLCanvasElement | null> = ref(null);
+
+let renderer: WebGLRenderer | null = null;
+let camera: PerspectiveCamera;
+const scene = new Scene();
+
+const makeInstance = (geometry: BufferGeometry, color: string | number, x: number) => {
+  const material = new MeshPhongMaterial({color});
+
+  const mesh = new Mesh(geometry, material);
+  scene.add(mesh);
+
+  mesh.position.x = x;
+
+  return mesh;
+}
+
+const meshes = [
+  makeInstance(new BoxGeometry(1, 1, 1), 0x44aa88, 0),
+  makeInstance(new BoxGeometry(1, 1, 1), 0x8844aa, -2),
+  makeInstance(new BoxGeometry(1, 1, 1), 0xaa8844, 2),
+];
+
+// LIGHT
+const light = new DirectionalLight(0xffffff, 3);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+function animate() {
+  if (!renderer) return;
+
+  meshes.forEach((mesh) => {
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+  });
+
+  renderer.render(scene, camera);
+}
+
+onMounted(() => {
+  if (!canvasRef.value) return;
+
+  camera = new PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+  );
+  camera.position.z = 5;
+
+  renderer = new WebGLRenderer({canvas: canvasRef.value});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animate);
+});
+
+const changeMeshColor = (index: number, e: Event) => {
+  meshes[index]?.material.color.set(e?.target?.value)
+}
+
+
+</script>
+
+<template>
+  <div>
+    <div class="controls">
+      <div v-for="(mesh, index) in meshes" :key="index" class="color-control">
+        <label for="color">Выберите цвет</label>
+        <input type="color" :value="`#${mesh.material.color.getHexString()}`" @input="(e) => changeMeshColor(index, e)">
+      </div>
+    </div>
+    <canvas ref="canvasRef"/>
+  </div>
+</template>
+
+<style>
+.controls {
+  position: absolute;
+
+  label {
+    color: #ffffff;
+  }
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,39 +1,11 @@
 <script setup lang="ts">
-import {Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, SphereGeometry, WebGLRenderer} from "three";
-
-const canvasRef: Ref<HTMLCanvasElement | null> = ref(null)
-let renderer: WebGLRenderer;
-const scene = new Scene();
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 2);
-
-scene.add(camera);
-
-const sphere = new Mesh(
-    new SphereGeometry(1, 32, 32),
-    new MeshBasicMaterial({color: 0x008080})
-)
-
-scene.add(sphere);
-
-
-const setRenderer = () => {
-  if (canvasRef.value) {
-    renderer = new WebGLRenderer({canvas: canvasRef.value,});
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.render(scene, camera);
-  }
-}
-
-onMounted(() => {
-  setRenderer();
-})
+import ThreeCanvas from "../components/ThreeCanvas.client.vue";
 </script>
 
 <template>
-  <div>
-    <canvas ref="canvasRef"/>
-  </div>
+  <main>
+    <three-canvas/>
+  </main>
 </template>
 
 <style scoped>
